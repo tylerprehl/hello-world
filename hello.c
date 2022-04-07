a8f4df3d703665416cd316c83ae4ad8e3d08abc2
Tyler Prehl <62523195+tylerprehl@users.noreply.github.com>
2022-04-07 09:04:39 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,8 +3,6 @@
 int main() {
    printf("Hello world! - 2022");
    int x = 1;
-   int y = 2;
-   int z = x + y;
-   printf("The sum of x and y is: %d\n", z);
+   printf("The sum of x and y is: %d\n", );
    return 0;
 }
